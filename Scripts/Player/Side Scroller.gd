c907a6f04c0d1422a4ffd5bc80b1b87536e0990c
Aaron Mathew <96519299+Aaron-230@users.noreply.github.com>
2026-03-01 11:23:32 +0530

    Added Basic Character Movement and Animation

---
+++ Scripts/Player/Side Scroller.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+const WALK_SPEED = 200.0
+const RUN_SPEED = 400.0
+const JUMP_VELOCITY = -450.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var Sprite = $Sprite
+
+func _physics_process(delta):
+	var direction = Input.get_axis("Left", "Right")
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("Jump"):
+		print(is_on_floor())
+	
+	var is_running = Input.is_action_pressed("Run")
+	var current_speed = RUN_SPEED if is_running else WALK_SPEED
+	
+	if direction:
+		velocity.x = direction * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+
+	move_and_slide()
+	
+	update_animations(direction, is_running)
+
+func update_animations(direction, is_running):
+	if direction != 0:
+		Sprite.flip_h = (direction < 0)
+	
+	if velocity.x == 0:
+		Sprite.play("Idle")
+	else:
+		if is_running:
+			Sprite.play("Running")
+		else:
+			Sprite.play("Walking")
